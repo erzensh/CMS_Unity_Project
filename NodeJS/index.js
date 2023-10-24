ae224093fc734a9058d433b20e3ae2d140b6ffaf
erzensh <erzenshabani22@gmail.com>
2023-10-24 11:42:23 +0200

    ww

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -3,28 +3,49 @@ const spdy = require('spdy');
 const mongoose = require('mongoose');
 const path = require('path');
 const fs = require('fs');
-const http2 = require('http2');
-
+const WebSocket = require('ws');
 
 const app = express();
 
 /* Configure Mongoose to Connect to MongoDB */
 mongoose.connect('mongodb://127.0.0.1:27017/cms', { useNewUrlParser: true })
-    .then(response => {
+    .then(() => {
         console.log("MongoDB connected successfully.");
     })
     .catch(err => {
-        console.log("Database connection failed.");
+        console.log("Database connection failed.", err);
     });
 
 // Enable HTTP/2
 const server = spdy.createServer({
-    key: fs.readFileSync('C:/Users/erzen/Documents/GitHub/CMS_Unity_Project/NodeJS/private-key.pem'),
-    cert: fs.readFileSync('C:/Users/erzen/Documents/GitHub/CMS_Unity_Project/NodeJS/certificate.pem'),
+    key: fs.readFileSync('E:/T-Unity-9/Documents/CMS_Unity_Project/NodeJS/private-key.pem'),
+    cert: fs.readFileSync('E:/T-Unity-9/Documents/CMS_Unity_Project/NodeJS/certificate.pem'),
 }, app);
 
+// Create a WebSocket server attached to the HTTP server
+const wss = new WebSocket.Server({ noServer: true });
 
+// Handle WebSocket connections
+wss.on('connection', (ws) => {
+    console.log('Client connected');
 
+    // Handle messages from Unity
+    ws.on('message', (message) => {
+        console.log(`Received message from Unity: ${message}`);
+
+        // Process the message or perform any other actions
+
+        // Send a response back to Unity
+        ws.send(JSON.stringify({ message: 'Text received successfully.' }));
+    });
+});
+
+// Upgrade an incoming HTTP request to a WebSocket connection
+server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit('connection', ws, request);
+    });
+});
 
 /* Configure Express */
 app.use(express.json());
@@ -32,11 +53,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 /* Routes */
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send("Welcome to the CMS App");
 });
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
